2c041340963ff104e838a4b80a93faaee06b7299
Omera <omeraxx7@gmail.com>
2019-03-25 16:40:09 +0200

    add except to git_test

--- git_test.py
+++ git_test.py
@@ -2,5 +2,8 @@ print("Hello World!")
 data = "My name is omer"
 words = data.split(' ')
 for word in words:
-    print(f"number of char in this word is: {len(word)}")
+    try:
+        print(f"number of char in this word is: {len(word)}")
+    except:
+        print("number of char in this word is: {}".format(len(word)))
     print(word)
