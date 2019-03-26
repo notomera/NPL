8af15bf73f5e3196fa38d5699cc4fccb0d765a7c
Omera <omeraxx7@gmail.com>
2019-03-26 14:03:59 +0200

    Remove everything for prod

--- git_test.py
+++
@@ -1,9 +0,0 @@
-print("Hello World!")
-data = input("Please type in something: ")
-words = data.split(' ')
-for word in words:
-    try:
-        print(f"number of char in this word is: {len(word)}")
-    except:
-        print("number of char in this word is: {}".format(len(word)))
-    print(word)
