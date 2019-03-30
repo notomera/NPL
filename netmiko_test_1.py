09b76ee23f5e2a86ea0ac12702924a26805a1adb
Omera <omeraxx7@gmail.com>
2019-03-30 12:15:21 +0200

    parse yaml input

--- netmiko_test_1.py
+++ netmiko_test_1.py
@@ -1,16 +1,46 @@
 from netmiko import ConnectHandler
+import yaml
 
-cisco = {
-    'device_type': 'cisco_ios',
-    'host': '10.100.0.100',
-    'username': 'npl',
-    'password': 'npl',
-    }
+# cisco = {
+#     'device_type': 'cisco_ios',
+#     'host': '10.100.0.100',
+#     'username': 'npl',
+#     'password': 'npl',
+#     'secret': 'npl'
+#     }
+#
+# R1 = ConnectHandler(**cisco)
+#
+# #Enter enable mode
+# R1.enable()
+#
+# #copy the running config to a file
+#
+# R1.send_command('terminal length 0')
+# R1_rc = R1.send_command('show run')
+# with open('R1_running_config.txt', 'w') as rc:
+#     rc.write(R1_rc)
+#
+#
+# R1.disconnect()
 
-R1 = ConnectHandler(**cisco)
-print(R1.find_prompt())         # Verify that you've an established SSH connection
+with open('input.yml') as stream:
+    data = yaml.load(stream, Loader=yaml.FullLoader)
+print(data)
+device1_data = data['devices']['device_1']
+print(device1_data)
 
-R1_output = R1.send_command("show ip int brief")
-print(R1_output)
-#play with the enable password see if netmiko will use the default credintial
-# R1 = ConnectHandler(device_type='cisco_ios', host='10.100.0.100', username='npl', password='npl')
+R1 = ConnectHandler(**device1_data)
+
+#Enter enable mode
+R1.enable()
+
+#copy the running config to a file
+
+R1.send_command('terminal length 0')
+R1_rc = R1.send_command('show run')
+with open('R1_int.txt', 'w') as rc:
+    rc.write(R1_rc)
+
+
+R1.disconnect()
