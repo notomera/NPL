3ec7b5f0d6df9201f42c8ea68d6be947880763ed
Omera <omeraxx7@gmail.com>
2019-03-30 12:17:43 +0200

    clean old code

--- netmiko_test_1.py
+++ netmiko_test_1.py
@@ -1,28 +1,6 @@
 from netmiko import ConnectHandler
 import yaml
 
-# cisco = {
-#     'device_type': 'cisco_ios',
-#     'host': '10.100.0.100',
-#     'username': 'npl',
-#     'password': 'npl',
-#     'secret': 'npl'
-#     }
-#
-# R1 = ConnectHandler(**cisco)
-#
-# #Enter enable mode
-# R1.enable()
-#
-# #copy the running config to a file
-#
-# R1.send_command('terminal length 0')
-# R1_rc = R1.send_command('show run')
-# with open('R1_running_config.txt', 'w') as rc:
-#     rc.write(R1_rc)
-#
-#
-# R1.disconnect()
 
 with open('input.yml') as stream:
     data = yaml.load(stream, Loader=yaml.FullLoader)
