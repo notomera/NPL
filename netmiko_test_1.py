da9a42fd4b6db939c789b7123d4669597bd2ba9b
Omera <omeraxx7@gmail.com>
2019-04-05 18:23:16 +0200

    add device type, command to send and credential to be sent to input file

--- netmiko_test_1.py
+++ netmiko_test_1.py
@@ -1,26 +1,59 @@
 from netmiko import ConnectHandler
 import yaml
+from copy import deepcopy
 
+# TODO:
+# -add the command to be sent _DONE
+# -type of the device _DONE
+# -password independent connection _DONE
+# -add a site name
+# -WHAT IF THERE IS NO ENABLE PASSWORD
+# -style the output with the device name and the command _DONE 50%
+# -Use asyncio for multitasking.
 
 with open('input.yml') as stream:
     data = yaml.load(stream, Loader=yaml.FullLoader)
 
+# print(f'this is the input data \n{data}')
+# GRABBING BASE DATA
 base_data = data['base']
+
+# GRABBING DEVICE DATA
 devices = data['devices']
-print(devices)
+# print(f'this is the devices data \n{devices}')
 
 for device, host in devices.items():
+    # print(f'this is the host data \n{host}')
+    # A COPY OF THE DATA THAT WOULD BE MANIPULATED MORE THAN ONCE
+    new_base_data = deepcopy(base_data)
+
+    # INIT THE DEVICE OUTPUT
+    device_ssh_data = ''
+
+    if 'username' in host and not host['username']: host.pop('username')
+    if 'password' in host and not host['password']: host.pop('password')
+    if 'secret' in host and not host['secret']: host.pop('secret')
+    if 'device_type' in host and not host['device_type']: host.pop('device_type')
+    if 'commands' in host and not host['commands']: host.pop('commands')
     # print(host)
-    base_data.update(host)
-    # print(base_data)
-    device_ssh = ConnectHandler(**base_data)
+    new_base_data.update(host)
+
+    # COMMANDS TO BE PASSED FOR DEVICES
+    commands = new_base_data.pop('commands').split(',')
+    new_commands = deepcopy(commands)
+    # print(f'this is the commands data \n{commands}')
+    # print(f'this is the new_base_data data \n{new_base_data}')
+    device_ssh = ConnectHandler(**new_base_data)
 
     device_name = device_ssh.find_prompt().split('>')[0]
 
     device_ssh.enable()
+
     device_ssh.send_command('terminal length 0')
-    device_ssh_rc = device_ssh.send_command('show run')
-    with open(f'{device_name}_running_config.txt', 'w') as rc:
-        rc.write(device_ssh_rc)
+    for command in new_commands:
+        device_ssh_data += '\n {0} {1} {0} \n {2} {3}'.format('*'*20, command, device_ssh.send_command(command), '\n'*5)
+    with open(f'{device_name}_data', 'w') as rc:
+        rc.write(device_ssh_data)
 
     device_ssh.disconnect()
+
